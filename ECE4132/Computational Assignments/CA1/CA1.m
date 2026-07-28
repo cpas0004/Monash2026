f89c103532d93d555690aa1f38073c5dfbfa4a40
cpas0004 <cpas0004@student.monash.edu>
2026-07-28 11:22:40 +1000

    first half of CA1

---
+++ ECE4132/Computational Assignments/CA1/CA1.m
@@ -0,0 +1,49 @@
+clear all; close all; clc;
+
+%% Step response
+x = stepResponse.time;
+y = stepResponse.data;
+
+plot(x, y);
+
+stepInfo = stepinfo(y, x, 'SettlingTimeThreshold', 0.02);
+settleTime = stepInfo.SettlingTime;
+
+steadyStateValue = y(find(x == ceil(settleTime)));
+initValue = y(1);
+
+fprintf("Steady state temp: %f\n", steadyStateValue);
+fprintf("Initial room temp: %f\n", initValue);
+
+%% 2 Modelling
+
+H = 100;
+syms C
+eqn = steadyStateValue - initValue == C * H;
+c = double(solve(eqn, C));
+
+fprintf("Constant C is: %f\n", c);
+
+T_ss = 40;
+syms H
+eqn = T_ss - initValue == c*H;
+h_40 = double(solve(eqn, H));
+
+fprintf("H for T_ss = 40: %f\n", h_40);
+
+x = ffResponse.time;
+y = ffResponse.data;
+
+stepInfo = stepinfo(y, x, 'SettlingTimeThreshold', 0.02);
+settleTime = stepInfo.SettlingTime;
+steadyStateValue = y(find(x == ceil(settleTime)));
+fprintf("Steady state temp for H = 32: %f\n", steadyStateValue);
+
+x = coolResponse.time;
+y = coolResponse.data;
+
+plot(x, y);
+xlabel("time (s)");
+ylabel("Temp (C)");
+
+
---
+++ ECE4132/Workshops/WS1/setupModelParametersWs1.m
@@ -0,0 +1,12 @@
+% setup model parameters
+clear;
+close all;
+
+% parameters for DC motor model (from
+% http://ctms.engin.umich.edu/CTMS/index.php?example=MotorPosition&section=SystemModeling)
+J = 3.2284E-6;
+D = 3.5077E-6;
+K = 0.0274;
+R = 4;
+% L = 2.75E-6;
+L = 5.5E-6;
