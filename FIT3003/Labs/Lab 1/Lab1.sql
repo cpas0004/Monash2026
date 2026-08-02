6e50aa5490093df6a69dd093d6b1bf21388e458d
cpas0004 <cpas0004@student.monash.edu>
2026-08-02 10:02:39 +1000

    Started lab 2

--- FIT3003/Labs/Lab 1/Lab1.sql
+++ FIT3003/Labs/Lab 1/Lab1.sql
@@ -1,23 +1,23 @@
 select *
   from tab;
 
--- CREATE TABLE LECTURER
--- (StaffNO 			NUMBER(6) 		NOT NULL, 
---  Title				VARCHAR2(3),
---  FName 				VARCHAR2(30),
---  LName				VARCHAR2(30),
---  StreetAddress		VARCHAR2(70), 
---  Suburb				VARCHAR2(40), 
---  City				VARCHAR2(40), 
---  PostCode			VARCHAR2(4), 
---  Country				VARCHAR2(30),
---  LecturerLevel		CHAR(2), 
---  BankNO				CHAR(20),
---  BankName			VARCHAR2(40),
---  Salary				NUMBER(8,2), 
---  WorkLoad			NUMBER(2,1) 	NOT NULL, 
---  ResearchArea			VARCHAR2(40),
---  PRIMARY KEY(StaffNo));
+CREATE TABLE LECTURER
+(StaffNO 			NUMBER(6) 		NOT NULL, 
+ Title				VARCHAR2(3),
+ FName 				VARCHAR2(30),
+ LName				VARCHAR2(30),
+ StreetAddress		VARCHAR2(70), 
+ Suburb				VARCHAR2(40), 
+ City				VARCHAR2(40), 
+ PostCode			VARCHAR2(4), 
+ Country				VARCHAR2(30),
+ LecturerLevel		CHAR(2), 
+ BankNO				CHAR(20),
+ BankName			VARCHAR2(40),
+ Salary				NUMBER(8,2), 
+ WorkLoad			NUMBER(2,1) 	NOT NULL, 
+ ResearchArea			VARCHAR2(40),
+ PRIMARY KEY(StaffNo));
 
 insert into lecturer (
    staffno,
