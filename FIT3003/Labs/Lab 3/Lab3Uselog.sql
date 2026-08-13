e2742508203367f5d4a144ede645a74d7c022a64
cpas0004 <cpas0004@student.monash.edu>
2026-08-13 13:09:45 +1000

    Finished Lab3A

--- FIT3003/Labs/Lab 3/Lab3Uselog.sql
+++ FIT3003/Labs/Lab 3/Lab3Uselog.sql
@@ -128,3 +128,121 @@ having count(*) > 1;
 
 --------------------------------------------
 -- PART C
+
+-- Using the DISTINCT keyword to remove duplicates for the tempfact table
+create table tempfact_uselog2 as
+select distinct U.log_date , U.log_time,
+U.student_ID, S.class_id, S.major_code
+from dw.uselog U, dw.student S
+where U.student_id = S.student_id;
+
+-- How many records are in the new tempfact table?
+select count(*) from tempfact_uselog2;
+select count(*) from dw.uselog;
+
+-- mostly fixed the issue, but there are still 6 extra records in uselog compared to tempfact_uselog2.
+
+-- Check for illegal students  in uselog
+select * from dw.USELOG
+where student_id not in (select student_id from dw.student);
+-- No, all good
+
+-- Check for illegal majors in dw.student
+select *
+from dw.uselog u, dw.student s
+where u.student_id = s.student_id
+and s.major_code not in (select major_code from dw.major);
+-- No, all good
+
+-- Did some uselog entries not get copied to tempfact_uselog2?
+-- Check if there are records in uselog that are not in tempfact_uselog2
+select *
+from dw.uselog u
+where log_date not in (select log_date from tempfact_uselog2)
+and log_time not in (select log_time from tempfact_uselog2)
+and student_id not in (select student_id from tempfact_uselog2);
+-- No, they are all in there
+
+-- Are there duplicates in uselog then?
+SELECT log_date, log_time, student_id, COUNT(*)
+FROM dw.uselog
+GROUP BY log_date, log_time, student_id
+HAVING COUNT(*) > 1;
+
+
+-- Now we repeat all the step to create the tempfact_uselog2 again
+alter table tempfact_uselog2
+add (timeid number);
+
+update tempfact_uselog2
+set timeid = 1
+where  to_char(log_time, 'HH24:MI') >= '06:01'
+and to_char(log_time, 'HH24:MI') <='12:00';
+
+select count(*) from tempfact_uselog2 where timeid = 1;
+
+update tempfact_uselog2
+set timeid = 2
+where  to_char(log_time, 'HH24:MI') >= '12:01'
+and to_char(log_time, 'HH24:MI') <='18:00';
+
+select count(*) from tempfact_uselog2 where timeid = 2;
+
+update tempfact_uselog2
+set timeid = 3
+where to_char(log_time, 'HH24:MI') >= '18:01'
+or to_char(log_time, 'HH24:MI') <='06:00';
+
+select count(*) from tempfact_uselog2 where timeid = 3;
+
+-- Adding the semid
+alter table tempfact_uselog2
+add (semid varchar2(10));
+
+update tempfact_uselog2
+set semid = 'S1'
+where to_char(log_date, 'MMDD') >= '0101'
+and to_char(log_date, 'MMDD') <= '0715';
+
+select count(*) from tempfact_uselog2 where semid = 'S1';
+
+update tempfact_uselog2
+set semid = 'S2'
+where to_char(log_date, 'MMDD') >= '0716'
+and to_char(log_date, 'MMDD') <= '1231';
+
+select count(*) from tempfact_uselog2 where semid = 'S2';
+
+-- Now can make the fact table
+create table fact_uselog2 as
+select t.semid, t.timeid, t.class_id,
+t.major_code, count(t.student_id) as total_usage
+from tempfact_uselog2 t
+group by t.semid, t.timeid, t.class_id, t.major_code;
+
+-- inspecting the old and new fact tables
+select * 
+from fact_uselog
+order by semid, timeid, class_id, major_code;
+
+select *
+from fact_uselog2
+order by semid, timeid, class_id, major_code;
+
+-- practice queries
+
+-- How many total usage for each time period?
+select f.timeID, t.timeDesc, sum(f.total_usage) as total_usage
+from fact_uselog2 f join timeDIM t on f.timeid = t.timeid
+group by f.timeID, t.timeDesc;
+
+-- How many users by time, major and class
+select f.timeID, f.major_code, f.class_id, sum(f.total_usage) as total_usage
+from fact_uselog2 f
+group by f.timeID, f.major_code, f.class_id;
+
+-- how many users by major and semester?
+select f.semID, f.major_code, m.major_name, sum(f.total_usage) as total_usage
+from fact_uselog2 f join majorDIM m on f.major_code = m.major_code
+group by f.semID, f.major_code, m.major_name;
+
