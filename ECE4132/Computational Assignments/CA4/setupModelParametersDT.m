9ef1209cbd442366dbaaa5907360ec6b0d8f6d07
cpas0004 <cpas0004@student.monash.edu>
2026-09-08 13:39:58 +1000

    CA4

--- ECE4132/Computational Assignments/CA4/setupModelParametersDT.m
+++ ECE4132/Computational Assignments/CA4/setupModelParametersDT.m
@@ -10,13 +10,35 @@ Dref = ref-yroom;
 P = tf([-tau/2, 1],[tau/2 1])*tf(K,[1 a]);
 
 %% values of PI controller parameters
-kp = 8.75;
-ki = 0.1;
+kp = 6.5;
+ki = 0.06;
 
 %% value of sampling period
 T = 10;
 
 %% discrete-time approximation of PI controller (uncomment and complete)
-% Cd = ;
+z = tf('z');
+Cd = kp + ki*T*(z+1)/(2*(z-1));
 
+%% Plotting
 
+DoutDTSS = DoutDT.data(end);
+
+plot(DoutCT.time, DoutCT.data);
+hold on
+plot(DoutDT.time, DoutDT.data);
+legend("CT", "DT");
+
+CTinfo = stepinfo(DoutCT.data, DoutCT.time, 7);
+DTinfo = stepinfo(DoutDT.data, DoutDT.time, 7);
+
+CTrise = CTinfo.RiseTime;
+CTsettle = CTinfo.SettlingTime;
+CTovershoot = CTinfo.Overshoot;
+
+DTrise = DTinfo.RiseTime;
+DTsettle = DTinfo.SettlingTime;
+DTovershoot = DTinfo.Overshoot;
+
+fprintf("For the CT system:\nRise Time: %.2f\nSettling Time: %.2f\nOvershoot: %.2f\n", CTrise, CTsettle, CTovershoot);
+fprintf("For the DT system:\nRise Time: %.2f\nSettling Time: %.4f\nOvershoot: %.2f\n", DTrise, DTsettle, DTovershoot);
--- ECE4132/Computational Assignments/CA4/setupModelParametersSat.m
+++ ECE4132/Computational Assignments/CA4/setupModelParametersSat.m
@@ -9,9 +9,25 @@ Dref = 60-yroom;
 
 P = tf([-tau/2, 1],[tau/2 1])*tf(K,[1 a]);
 
+
+
 %% values of PI controller parameters
 kp = 8.75;
 ki = 0.1;
 
+%% plotting
+
+plot(DoutModel.Time, DoutModel.Data);
+hold on
+plot(Dout.Time, Dout.Data);
+
+%%
+ModelSS = DoutModel.data(end);
+
+stepinfo(DoutModel.data, DoutModel.time, 37, RiseTimeLimits=[0.1, 0.9], SettlingTimeThreshold=0.05)
+
 %% value for back calculation parameter (uncomment and complete)
-% kt = ...;
+kt = 2*ki/kp;
+
+%%
+stepinfo(Dout.Data, Dout.Time, 37, RiseTimeLimits=[0.1, 0.9], SettlingTimeThreshold=0.05)
